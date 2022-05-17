c6abc407862684b3e1dd866af3583ca124a5167e
Patrick96 <gayratjonkamolov96@gmail.com>
2022-05-17 12:08:09 +0500

    data.sql da region va district lar qoshildi

--- src/main/java/uz/pdp/heymasterapp/entity/location/Location.java
+++ src/main/java/uz/pdp/heymasterapp/entity/location/Location.java
@@ -1,13 +1,10 @@
-package uz.pdp.heymasterapp.entity;
+package uz.pdp.heymasterapp.entity.location;
 
 
 import lombok.*;
 import uz.pdp.heymasterapp.entity.template.AbsEntity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,9 +17,11 @@ public class Location {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer Id;
 
-    private String region;
+    @ManyToOne
+    private Region region;
 
-    private String city;
+    @ManyToOne
+    private District district;
 
     private String street;
 
