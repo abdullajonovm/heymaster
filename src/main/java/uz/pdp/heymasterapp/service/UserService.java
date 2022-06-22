dc2d94c4797eff7528e07f15bb6d002ecd2543ba
One_Notebook <azizbekabdulaxatov338@gmail.com>
2022-06-22 12:29:26 +0500

    sdsfsdfsdf

--- src/main/java/uz/pdp/heymasterapp/service/UserService.java
+++ src/main/java/uz/pdp/heymasterapp/service/UserService.java
@@ -109,7 +109,7 @@ public class UserService {
     }
 
    public ApiResponse editMaster(User user, RegisterForMasterDto dto) {
-        if (user.getPhoneNumber()!= dto.getPhoneNumber()) {
+        if (!user.getPhoneNumber().equals( dto.getPhoneNumber()) ){
             Optional<User> optionalUser = userRepository.findByPhoneNumber(dto.getPhoneNumber());
             if (optionalUser.isPresent()) return new ApiResponse("This number already exist ", false);
         }
