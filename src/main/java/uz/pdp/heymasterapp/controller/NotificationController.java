77fbe27422938634732b0105bb7d4ac889d18e6c
One_Notebook <azizbekabdulaxatov338@gmail.com>
2022-06-28 18:38:53 +0500

    notification get for client and master

---
+++ src/main/java/uz/pdp/heymasterapp/controller/NotificationController.java
@@ -0,0 +1,58 @@
+package uz.pdp.heymasterapp.controller;
+
+import com.google.protobuf.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uz.pdp.heymasterapp.dto.ApiResponse;
+import uz.pdp.heymasterapp.entity.Notification;
+import uz.pdp.heymasterapp.entity.User;
+import uz.pdp.heymasterapp.repository.NotificationRepository;
+import uz.pdp.heymasterapp.security.CurrentUser;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/notification")
+public class NotificationController {
+
+    final NotificationRepository repository;
+    ApiResponse apiResponse=new ApiResponse();
+
+    @PreAuthorize("hasAnyAuthority('CLIENT')")
+    @GetMapping("/user/all")
+    public ResponseEntity getAllClient(@CurrentUser User user){
+        Optional<List<Notification>> optionalList = repository.findAllByCreatedBy(user.getId());
+        if (optionalList.isPresent()){
+            apiResponse.setSuccess(true);
+            apiResponse.setObject(optionalList.get());
+            apiResponse.setMessage("ok");
+            return ResponseEntity.ok(apiResponse);
+        }
+        apiResponse.setSuccess(false);
+
+        apiResponse.setMessage("not notification ");
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @PreAuthorize("hasAnyAuthority('MASTER')")
+    @GetMapping("/master/all")
+    public ResponseEntity getAllMaster(@CurrentUser User user){
+        Optional<List<Notification>> optionalList = repository.findAllByToWhom(user);
+        if (optionalList.isPresent()){
+            apiResponse.setSuccess(true);
+            apiResponse.setObject(optionalList.get());
+            apiResponse.setMessage("ok");
+            return ResponseEntity.ok(apiResponse);
+        }
+        apiResponse.setSuccess(false);
+
+        apiResponse.setMessage("not notification ");
+        return ResponseEntity.ok(apiResponse);
+    }
+}
--- src/main/java/uz/pdp/heymasterapp/repository/NotificationRepository.java
+++ src/main/java/uz/pdp/heymasterapp/repository/NotificationRepository.java
@@ -3,7 +3,14 @@ package uz.pdp.heymasterapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uz.pdp.heymasterapp.entity.Notification;
+import uz.pdp.heymasterapp.entity.User;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    Optional<List<Notification>>findAllByCreatedBy(Long createdBy);
+    Optional<List<Notification>>findAllByToWhom(User toWhom);
 }
